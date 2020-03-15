606ff9bd68004189827c952f7e490f37dcc87424
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-15 08:21:50 -0300

    allow remote access through cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 import './database';
 
@@ -15,6 +16,7 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(cors());
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
