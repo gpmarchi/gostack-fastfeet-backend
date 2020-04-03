a58c5c4411e5bcb60bfb593b9b2e8aebcc4fede9
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-04-03 15:03:09 -0300

    add sentry monitoring and exception middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+import Youch from 'youch';
+
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -10,11 +15,15 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(cors());
     this.server.use(
@@ -25,6 +34,19 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (error, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(error, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error.' });
+    });
   }
 }
 
