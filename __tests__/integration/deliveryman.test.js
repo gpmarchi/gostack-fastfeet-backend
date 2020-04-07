cd43370914f28be2b89d54a1cb14073d6c5d229b
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-04-07 14:36:16 -0300

    truncate all tables before tests executions

--- __tests__/integration/deliveryman.test.js
+++ __tests__/integration/deliveryman.test.js
@@ -1,17 +1,28 @@
 import request from 'supertest';
+import jwt from 'jsonwebtoken';
+
+import '../../src/bootstrap';
+import authConfig from '../../src/config/auth';
+import truncate from '../util/truncate';
 import app from '../../src/app';
 
+const token = jwt.sign({ id: 1 }, authConfig.secret, {
+  expiresIn: authConfig.expiresIn,
+});
+
 describe('Deliveryman', () => {
-  it('should be able to register a new deliveryman', async done => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should be able to register a new deliveryman', async () => {
     const response = await request(app)
       .post('/deliverymen')
-      .set('Authorization', `Bearer ${process.env.JWT_TOKEN}`)
+      .set('Authorization', `Bearer ${token}`)
       .send({
         name: 'Deliveryman one',
         email: 'deliverymanone@gmail.com',
       });
-
-    done();
 
     expect(response.body).toHaveProperty('id');
   });
